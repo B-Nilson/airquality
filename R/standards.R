4ab467eadfbbbe8fc0026ed8865c417317bb8db6
Brayden Nilson <nilson.brayden@gmail.com>
2024-05-25 23:57:16 -0700

    Create initial CAAQS function

--- R/standards.R
+++ R/standards.R
@@ -144,3 +144,198 @@ AQHI_plus = function(
   )
 
 }
+
+# CAAQS -------------------------------------------------------------------
+
+CAAQS = function(datetimes, pm25_hourly = NULL, o3_hourly = NULL,
+                 no2_hourly = NULL, so2_hourly = NULL){
+  require(dplyr)
+  require(lubridate)
+  require(tidyr)
+
+  # TODO: Ensure 3 years of data provided
+
+  # Define thresholds for each pollutant / avg / year
+  thresholds = list(
+    pm25 = list(
+      daily  = c('2015' = 28, '2020' = 27),
+      annual = c('2015' = 10, '2020' = 8.8)
+    ),
+    o3 = list(
+      `8hr`  = c('2015' = 63, '2020' = 62, '2025' = 60)
+    ),
+    no2 = list(
+      hourly = c('2020' = 60, '2025' = 42),
+      annual = c('2020' = 17, '2025' = 12)
+    ),
+    so2 = list(
+      hourly = c('2020' = 60, '2025' = 42),
+      annual = c('2020' = 17, '2025' = 12)
+    )
+  )
+  # helper functions to replace NA/inf with val
+  swap_na = function(x, val = -99) ifelse(is.na(x), val, x)
+  swap_inf = function(x, val = NA) ifelse(is.infinite(x), val, x)
+  # helper functions to remove NA by default
+  mean_no_na = function(x, ...) mean(x, na.rm = T, ...)
+  max_no_na = function(x, ...) swap_inf(suppressWarnings(max(x, na.rm = T, ...)), NA)
+  # helper function to extract relevant thresholds
+  get_threshold = function(thresholds, year){
+    out = thresholds %>%
+      .[as.numeric(names(.)) <= year] %>%
+      last()
+    if(length(out) == 0) out = NA
+    return(out)
+  }
+  # helper function to make 3 year means
+  get_lag_n_mean = function(x, n = 3){
+    out = x
+    if(n <= 1) stop("`n` must be greater than one")
+    for(i in 1:(n-1)){
+      out = out + lag(x, i)
+    }
+    return(out/n)
+  }
+  # Join inputs
+  obs = bind_cols(
+    date = datetimes, pm25 = pm25_hourly,
+    o3 = o3_hourly, no2 = no2_hourly,
+    so2 = so2_hourly
+  ) %>%
+    # Fill in missing hours with NAs
+    complete(date = seq(lubridate::floor_date(min(date), "years"),
+                        lubridate::ceiling_date(max(date), "years") - lubridate::hours(1),
+                             "1 hours")) %>%
+    arrange(date)
+
+  # Get daily mean and max
+  daily = obs %>%
+    group_by(date = lubridate::floor_date(date, "days")) %>%
+    summarise(across(everything(), c(mean = mean_no_na, max = max_no_na)))
+
+  met = list()
+  # Test pm25 if provided
+  if(!is.null(pm25_hourly)){
+    met$pm25 = daily %>%
+      # Daily mean -> annual 98th percentile and annual mean
+      group_by(year = year(date)) %>%
+      summarise(p_complete = round(sum(!is.na(pm25_mean)) / n(), 3)*100,
+                perc_98_of_daily_means = quantile(pm25_mean, 0.98, na.rm = T),
+                mean_of_daily_means = mean(pm25_mean, na.rm = T)) %>%
+      # +3 year averages, +standard for that year, +whether standard is met
+      mutate(
+        # +3 year averages,
+        `3yr_mean_of_perc_98` = get_lag_n_mean(perc_98_of_daily_means, n = 3),
+        `3yr_mean_of_means` = get_lag_n_mean(mean_of_daily_means, n = 3),
+        # +standards for that year
+        daily_standard = sapply(year, get_threshold,
+                                thresholds = thresholds$pm25$daily),
+        annual_standard = sapply(year, get_threshold,
+                                 thresholds = thresholds$pm25$annual),
+        # +whether standards are met
+        meets_standard_daily = `3yr_mean_of_perc_98` <= daily_standard,
+        meets_standard_annual = `3yr_mean_of_means` <= annual_standard
+      ) %>%
+      dplyr::relocate(c(daily_standard, meets_standard_daily),
+                      .after = "3yr_mean_of_perc_98")%>%
+      dplyr::relocate(mean_of_daily_means,
+                      .after = "meets_standard_daily") %>%
+      dplyr::relocate(p_complete, .after = "year")
+  }
+  # Test o3 if provided
+  if(!is.null(o3_hourly)){
+    met$o3 = obs %>%
+      # hourly mean -> 8 hourly mean
+      group_by(date = floor_date(date, "8 hours")) %>%
+      summarise(`8hr_mean_o3` = mean(o3, na.rm = T)) %>%
+      # 8 hourly mean -> daily max
+      group_by(date = floor_date(date, "days")) %>%
+      summarise(daily_max_8hr_mean_o3 = max_no_na(`8hr_mean_o3`)) %>%
+      # daily max -> annual 4th highest
+      group_by(year = year(date)) %>%
+      arrange(desc(daily_max_8hr_mean_o3)) %>%
+      summarise(
+        p_complete = round(sum(!is.na(daily_max_8hr_mean_o3)) / n(), 3)*100,
+        fourth_highest_daily_max_8hr_mean_o3 = daily_max_8hr_mean_o3[4]) %>%
+      # +3 year averages, +standard for that year, +whether standard is met
+      mutate(
+        # +3 year averages
+        `3yr_mean` = get_lag_n_mean(fourth_highest_daily_max_8hr_mean_o3, n = 3),
+        # +standard for that year
+        standard = sapply(year, get_threshold,
+                                 thresholds = thresholds$o3$`8hr`),
+        # +whether standard is met
+        meets_standard = `3yr_mean` <= standard
+      ) %>%
+      dplyr::relocate(p_complete, .after = "year")
+  }
+  # Test no2 if provided
+  if(!is.null(no2_hourly)){
+    met$no2 = obs %>%
+      # + annual mean
+      group_by(year = year(date)) %>%
+      mutate(annual_mean_of_hourly = mean(no2, na.rm = T)) %>%
+      # hourly mean -> daily maxima
+      group_by(date = floor_date(date, "1 days"), annual_mean_of_hourly) %>%
+      summarise(daily_max_hourly_no2 = max(no2, na.rm = T), .groups = "drop") %>%
+      # daily maxima -> annual 98th percentile
+      group_by(year = year(date), annual_mean_of_hourly) %>%
+      summarise(
+        p_complete = round(sum(!is.na(daily_max_hourly_no2)) / n(), 3)*100,
+        perc_98_of_daily_maxima = quantile(
+          daily_max_hourly_no2, 0.98, na.rm = T), .groups = "drop") %>%
+      # +3 year averages, +standard for that year, +whether standard is met
+      mutate(
+        # +3 year averages
+        `3yr_mean_of_perc_98` = get_lag_n_mean(perc_98_of_daily_maxima, n = 3),
+        # +standards for that year
+        standard_hourly = sapply(year, get_threshold,
+                                 thresholds = thresholds$no2$hourly),
+        standard_annual = sapply(year, get_threshold,
+                                 thresholds = thresholds$no2$annual),
+        # +whether standard is met
+        meets_standard_hourly = annual_mean_of_hourly <= standard_hourly,
+        meets_standard_annual = `3yr_mean_of_perc_98` <= standard_annual,
+      ) %>%
+      dplyr::relocate(c(standard_hourly, meets_standard_hourly),
+                      .after = "annual_mean_of_hourly") %>%
+      dplyr::relocate(p_complete, .after = "year")
+  }
+  # Test so2 if provided
+  if(!is.null(so2_hourly)){
+    met$so2 = obs %>%
+      # + annual mean
+      group_by(year = year(date)) %>%
+      mutate(annual_mean_of_hourly = mean(so2, na.rm = T)) %>%
+      # hourly mean -> daily maxima
+      group_by(date = floor_date(date, "1 days"), annual_mean_of_hourly) %>%
+      summarise(daily_max_hourly_so2 = max(no2, na.rm = T), .groups = "drop") %>%
+      # daily maxima -> annual 98th percentile
+      group_by(year = year(date), annual_mean_of_hourly) %>%
+      summarise(
+        p_complete = round(sum(!is.na(daily_max_hourly_so2)) / n(), 3)*100,
+        perc_99_of_daily_maxima = quantile(
+          daily_max_hourly_so2, 0.99, na.rm = T), .groups = "drop") %>%
+      # +3 year averages, +standard for that year, +whether standard is met
+      mutate(
+        # +3 year averages
+        `3yr_mean_of_perc_99` = get_lag_n_mean(perc_99_of_daily_maxima, n = 3),
+        # +standards for that year
+        standard_hourly = sapply(year, get_threshold,
+                                 thresholds = thresholds$so2$hourly),
+        standard_annual = sapply(year, get_threshold,
+                                 thresholds = thresholds$so2$annual),
+        # +whether standard is met
+        meets_standard_hourly = annual_mean_of_hourly <= standard_hourly,
+        meets_standard_annual = `3yr_mean_of_perc_99` <= standard_annual,
+      ) %>%
+      dplyr::relocate(c(standard_hourly, meets_standard_hourly),
+                      .after = "annual_mean_of_hourly") %>%
+      dplyr::relocate(p_complete, .after = "year")
+  }
+}
+
+
+
+
+
