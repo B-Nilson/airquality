4d19e482ec83f0d4f9b2ad0173650ca46ce1caa1
Brayden Nilson <nilson.brayden@gmail.com>
2024-05-26 14:53:38 -0700

    Add documentation for AQHI_plus()

--- R/standards.R
+++ R/standards.R
@@ -128,7 +128,43 @@ AQHI = function(datetimes, pm25_hourly, no2_hourly = NA, o3_hourly = NA, quiet =
   return(obs)
 }
 
-# Calculates Canadian AQHI+ (PM2.5 only)
+
+#' Calculate the Canadian AQHI+ from hourly PM2.5 observations
+#'
+#' @param pm25_hourly A numeric/integer vector with hourly mean PM2.5 concentrations (ug/m^3).
+#' @param min_allowed_pm25 A single numeric value indicating the minimum allowed concentration (Defaults to 0 ug/m^3). All values in `pm25_hourly` less than this will be replaced with NA.
+#'
+#' @description
+#' The Canadian AQHI+ is a modification of the Canadian Air Quality Health Index (AQHI).
+#' AQHI+ only uses fine particulate matter (PM2.5) instead of the combination of PM2.5, ozone (O3), and nitrogen dioxide (NO2)
+#' , and is calculated using hourly means instead of using 3 hour means.
+#' The AQHI+ overrides the AQHI if it exceeds the AQHI, which typically occurs during wildfire smoke events.
+#'
+#' Hourly PM2.5 is split into bins of 10 ug/m^3 up to 100 ug/m^3 (AQHI+ 1 - 10)
+#' , and values greater than 100 ug/m^3 have an AQHI+ of "+".
+#' The risk categories match the AQHI Low [1-3, or 0-30 ug/m^3], Moderate [4-6, or 30.1-60 ug/m^3]
+#' , High [7-10, or 60.1-100 ug/m^3], and Very High [+, or >100 ug/m^3] and share the same health messaging.
+#'
+#' AQHI+ was originally published in Yao et. al (2019) DOI: https://doi.org/10.17269/s41997-019-00237-w.
+#' And has been adopted by all Canadian provinces/territories as of 2024 (except Quebec where they use the AQI instead of the AQHI/AQHI+).
+#'
+#' @return A tibble (data.frame) with columns:
+#' hourly_pm25, AQHI_plus, risk, high_risk_pop_message, general_pop_message
+#' and `length(pm25_hourly)` rows
+#' @export
+#'
+#' @examples
+#' obs = data.frame(
+#'   date = seq(lubridate::ymd_h("2024-01-01 00"),
+#'              lubridate::ymd_h("2024-01-01 23"), "1 hours"),
+#'   pm25 = sample(1:150, 24))
+#' AQHI_plus(obs$pm25)
+#'
+#' obs = data.frame(
+#'   date = seq(lubridate::ymd_h("2024-01-01 00"),
+#'              lubridate::ymd_h("2024-01-01 23"), "1 hours"),
+#'   pm25 = c(-2, -0.1, sample(1:150, 22))
+#' AQHI_plus(obs$pm25, min_allowed_pm25 = -0.5)
 AQHI_plus = function(
     pm25_hourly,
     min_allowed_pm25 = 0){
