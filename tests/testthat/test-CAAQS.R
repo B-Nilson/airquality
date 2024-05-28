7573470042f89ddd1d7804a5dea08f0ef50fc3ec
Brayden Nilson <nilson.brayden@gmail.com>
2024-05-27 21:17:36 -0700

    Init tests for CAAQS (needs more)

---
+++ tests/testthat/test-CAAQS.R
@@ -0,0 +1,13 @@
+obs = data.frame(
+  date = seq(lubridate::ymd_h("2020-01-01 00"),
+             lubridate::ymd_h("2023-12-31 23"), "1 hours"),
+  pm25 = sample(1:150, 35064, T), o3 = sample(1:150, 35064, T),
+  no2 = sample(1:150, 35064, T), so2 = sample(1:150, 35064, T)
+)
+
+test_that("Returns a list of proper size", {
+  output = CAAQS(datetimes = obs$date, pm25_hourly = obs$pm25,
+                o3_hourly = obs$o3, so2_hourly = obs$so2)
+  expect_type(output, "list")
+  expect_length(output, 4)
+})
