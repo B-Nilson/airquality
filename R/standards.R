69adc00f413ea813bb63971176ae601b08f26b89
Brayden Nilson <nilson.brayden@gmail.com>
2024-06-13 17:19:00 -0700

    Improve AQHI and AQHI+ documentation

--- R/standards.R
+++ R/standards.R
@@ -80,10 +80,10 @@ AQHI_replace_w_AQHI_plus = function(obs, aqhi_plus){
 
 #' Calculate Canadian AQHI from hourly PM2.5, NO2, and O3 observations
 #'
-#' @param datetimes Vector of hourly datetime values corresponding to observations. Date gaps will be filled automatically.
-#' @param pm25_hourly Vector of hourly mean fine particulate matter (PM2.5) concentrations (ug/m^3).
-#' @param no2_hourly (Optional). Vector of hourly mean nitrogen dioxide (NO2) concentrations (ppb). If not provided AQHI+ will be calculated from PM2.5 only.
-#' @param o3_hourly (Optional). Vector of hourly mean ozone (O3) concentrations (ppb). If not provided AQHI+ will be calculated from PM2.5 only.
+#' @param datetimes Vector of hourly datetimes corresponding to observations. Date gaps will be filled automatically.
+#' @param pm25_hourly Numeric vector of hourly mean fine particulate matter (PM2.5) concentrations (ug/m^3).
+#' @param no2_hourly (Optional). Numeric vector of hourly mean nitrogen dioxide (NO2) concentrations (ppb). If not provided AQHI+ will be calculated from PM2.5 only.
+#' @param o3_hourly (Optional). Numeric vector of hourly mean ozone (O3) concentrations (ppb). If not provided AQHI+ will be calculated from PM2.5 only.
 #' @param quiet (Optional). A single logical (TRUE/FALSE) value indicating if AQHI+ warning (if o3 and no2 not provided) should be hidden. Default is FALSE
 #'
 #' @description
@@ -93,13 +93,14 @@ AQHI_replace_w_AQHI_plus = function(obs, aqhi_plus){
 #' from a time-series analysis of air pollution and mortality in Canadian cities,
 #' adjusted to a 0–10 scale, and calculated hourly on the basis of trailing 3-hr average pollutant concentrations."
 #'
-#' The Canadian AQHI+ is a modification of the Canadian Air Quality Health Index (AQHI).
-#' AQHI+ only uses fine particulate matter (PM2.5) instead of the combination of PM2.5, ozone (O3), and nitrogen dioxide (NO2)
-#' , and is calculated using hourly means instead of using 3 hour means.
-#' The AQHI+ overrides the AQHI if it exceeds the AQHI, which typically occurs during wildfire smoke events.
+#' The AQHI is overriden by the Canadian AQHI+ if the AQHI+ exceeds the AQHI for a particular hour.
+#' The AQHI+ is a modification of the Canadian Air Quality Health Index (AQHI).
+#' AQHI+ only uses fine particulate matter (PM2.5) instead of the combination of PM2.5, ozone (O3), and nitrogen dioxide (NO2).
+#' Unlike the AQHI which uses 3-hour mean averages, AQHI+ is calculated using hourly means.
 #'
 #' The AQHI was originally published by Steib et. al in 2008,
-#'  and has been adopted by all Canadian provinces/territories (except Quebec where they use the AQI instead of the AQHI/AQHI+).
+#' and has been adopted by all Canadian provinces/territories
+#' (except Quebec where they use the AQI instead of the AQHI/AQHI+).
 #'
 #' @references \link{https://doi.org/10.3155/1047-3289.58.3.435}
 #'
@@ -181,17 +182,19 @@ AQHI = function(datetimes, pm25_hourly, no2_hourly = NA, o3_hourly = NA, quiet =
 #'
 #' @description
 #' The Canadian AQHI+ is a modification of the Canadian Air Quality Health Index (AQHI).
-#' AQHI+ only uses fine particulate matter (PM2.5) instead of the combination of PM2.5, ozone (O3), and nitrogen dioxide (NO2)
-#' , and is calculated using hourly means instead of using 3 hour means.
-#' The AQHI+ overrides the AQHI if it exceeds the AQHI, which typically occurs during wildfire smoke events.
+#' AQHI+ is meant for augmenting the AQHI to provide a more responsive health index during wildfire smoke events.
+#' AQHI+ only uses fine particulate matter (PM2.5) instead of the combination of PM2.5, ozone (O3), and nitrogen dioxide (NO2).
+#' In addition, AQHI+ is calculated using hourly mean averages instead of 3-hourly mean averages used by the AQHI.
+#' The AQHI+ overrides the AQHI if it exceeds the AQHI for a particular hour.
 #'
-#' Hourly PM2.5 is split into bins of 10 ug/m^3 up to 100 ug/m^3 (AQHI+ 1 - 10)
-#' , and values greater than 100 ug/m^3 have an AQHI+ of "+".
-#' The risk categories match the AQHI Low [1-3, or 0-30 ug/m^3], Moderate [4-6, or 30.1-60 ug/m^3]
-#' , High [7-10, or 60.1-100 ug/m^3], and Very High [+, or >100 ug/m^3] and share the same health messaging.
+#' AQHI+ splits hourly PM2.5 concentrations into bins of 10 ug/m^3 from 0 to 100 ug/m^3 (AQHI+ 1 - 10),
+#' and assigns "+" to values greater than 100 ug/m^3.
+#' The risk categories match the AQHI (Low [1-3, or 0-30 ug/m^3], Moderate [4-6, or 30.1-60 ug/m^3],
+#' High [7-10, or 60.1-100 ug/m^3], and Very High [+, or >100 ug/m^3]) and share the same health messaging.
 #'
 #' The AQHI+ was originally published by Yao et. al in 2019,
-#'  and has been adopted by all Canadian provinces/territories as of 2024 (except Quebec where they use the AQI instead of the AQHI/AQHI+).
+#' and has been adopted by all Canadian provinces/territories as of 2024
+#' (except Quebec where they use the AQI instead of the AQHI and AQHI+).
 #'
 #' @references \link{https://doi.org/10.17269/s41997-019-00237-w}
 #'
@@ -204,16 +207,20 @@ AQHI = function(datetimes, pm25_hourly, no2_hourly = NA, o3_hourly = NA, quiet =
 #' @export
 #'
 #' @examples
+#' # Hourly pm2.5 concentrations
 #' obs = data.frame(
 #'   date = seq(lubridate::ymd_h("2024-01-01 00"),
 #'              lubridate::ymd_h("2024-01-01 23"), "1 hours"),
 #'   pm25 = sample(1:150, 24))
+#' # Calculate the AQHI+
 #' AQHI_plus(obs$pm25)
 #'
+#' # Hourly pm2.5 concentrations (with negative values)
 #' obs = data.frame(
 #'   date = seq(lubridate::ymd_h("2024-01-01 00"),
 #'              lubridate::ymd_h("2024-01-01 23"), "1 hours"),
 #'   pm25 = c(-2, -0.1, sample(1:150, 22)))
+#' # Calculate the AQHI+ for each hour, except for hours where pm2.5 is < -0.5
 #' AQHI_plus(obs$pm25, min_allowed_pm25 = -0.5)
 #' @importFrom rlang .data
 AQHI_plus = function(pm25_hourly, min_allowed_pm25 = 0){
