ea69091738bf933ec4d411a03dbab292e944dbd4
Brayden Nilson <nilson.brayden@gmail.com>
2024-06-15 18:27:13 -0700

    Add AQI() test

--- tests/testthat/test-AQI.R
+++ tests/testthat/test-AQI.R
@@ -3,6 +3,9 @@ test_that("AQI for O3 1hr only is correct", {
   expect_equal(AQI(Sys.time(), o3_8hr_ppm = 0.07853333)$AQI, 126)
 })
 
+test_that("Throws error when no data provided", {
+  expect_error(AQI())
+})
 
 test_that("AQI for O3 1hr and O3 8hr is correct", {
   expect_equal(AQI(Sys.time(), o3_8hr_ppm = 0.078, o3_1hr_ppm = 0.162)$AQI, 148)
