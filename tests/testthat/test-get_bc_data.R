7a370ec600d5c85b4f0afd9ee624b1702c208c0f
Brayden Nilson <nilson.brayden@gmail.com>
2024-06-25 13:48:19 -0700

    Use later dates to avoid getting 2 years of data in tests for get_bc_data() (for time saving)

--- tests/testthat/test-get_bc_data.R
+++ tests/testthat/test-get_bc_data.R
@@ -1,12 +1,12 @@
 test_that("returns requested stations only", {
-  obs = get_bc_data("0450307", "2019-01-01 00")
+  obs = get_bc_data("0450307", "2019-02-01 00")
   expect_true(all(unique(obs$site_id) %in% "0450307"))
-  obs = get_bc_data(c("0450307", "E206898"), "2019-01-01 00")
+  obs = get_bc_data(c("0450307", "E206898"), "2019-02-01 00")
   expect_true(all(unique(obs$site_id) %in% c("0450307", "E206898")))
 })
 
 test_that("all dates non-na and within requested period", {
-  date_range = lubridate::ymd_h(c("2019-01-01 00", "2019-01-02 00"), tz = "America/Toronto")
+  date_range = lubridate::ymd_h(c("2019-02-01 00", "2019-02-02 00"), tz = "America/Toronto")
   obs = get_bc_data("0450307", date_range)
   expect_true(all(!is.na(obs$date_utc)))
   expect_true(all(!is.na(obs$date_local)))
@@ -39,6 +39,6 @@ test_that("invalid date_range causes error/warning", {
 })
 
 test_that("unknown stations cause warning", {
-  expect_warning(get_bc_data("bananas", date_range = "2019-01-02 00"))
-  expect_warning(get_bc_data(c("bananas", "0450307"), date_range = "2019-01-02 00"))
+  expect_warning(get_bc_data("bananas", date_range = "2019-02-02 00"))
+  expect_warning(get_bc_data(c("bananas", "0450307"), date_range = "2019-02-02 00"))
 })
