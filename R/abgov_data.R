4d267d667b0bc26caeb3d7291972bc1a94010578
Brayden Nilson <45985474+B-Nilson@users.noreply.github.com>
2024-09-05 20:57:47 -0700

    Write documentation for abgov stations

--- R/abgov_data.R
+++ R/abgov_data.R
@@ -1,3 +1,30 @@
+#' Download air quality station metadata from the Alberta (Canada) Government
+#'
+#' @param use_sf (Optional) a single logical (TRUE/FALSE) value indicating whether or not to return a spatial object. using the `sf` package
+#' 
+#' @description
+#' Air pollution monitoring in Canada is done by individual Provinces/Territories,
+#' primarily as a part of the federal National Air Pollution Surveillance (NAPS) program.
+#' The Province of Alberta hosts it's hourly air quality metadata
+#' through a public API.
+#'
+#' [get_abgov_stations()] provides an easy way to retrieve this metadata (typically to determine station names to pass to `get_abgov_data()`)
+#'
+#' @seealso [get_abgov_data()]
+#' @return
+#' A tibble of station metadata for Alberta FEMs.
+#'
+#' @family Data Collection
+#' @family Canadian Air Quality
+#'
+#' @export
+#' @examples
+#' \donttest{
+#' # Normal usage
+#' get_abgov_stations()
+#' # if spatial object required
+#' get_abgov_stations(use_sf = TRUE)
+#' }
 get_abgov_stations = function(use_sf = FALSE){
   # Define endpoint
   api_endpoint = "Stations?"
