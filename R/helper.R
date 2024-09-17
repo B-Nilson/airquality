70b024c015109120feaa6daece4fbee3b53e6bde
Brayden Nilson <nilson.brayden@gmail.com>
2024-09-17 15:41:04 -0700

    Adjust save_figuer to use a standard width and adjust height as needed

--- R/helper.R
+++ R/helper.R
@@ -2,7 +2,8 @@
 #'
 #' @param gg a single ggplot object you would like to save.
 #' @param out_path A single character value indicating where to save the plot to (i.e. "./plots/my_figure_name.png")
-#' @param wider,taller A single numeric value indicating how many inches to add to the output plot width/height. Default makes a 5"x7" image.
+#' @param taller A single numeric value indicating how many inches to add/remove to the output plot height. Default makes a 5"x6.5" image.
+#' @param page_width A single numeric value indicating the width of the page (minus margins) in inches that the image is intended for.
 #' @param quality A single character value equal to "high" (dpi = 300), "medium" (dpi = 200), or "low" (dpi = 100) indicating the output quality of the figure. 
 #'   Text sizes may need to be adjusted for differenct quality levels. 
 #' @param ... (Optional) addition arguments passed on to ggplot2::ggsave()
@@ -24,12 +25,12 @@
 #'     data = data.frame(x = 1:100, y = (0:99)^2), 
 #'     ggplot2::aes(x, y))
 #' # save_figure(gg, "./test.png", taller = 1)
-save_figure = function(gg, out_path, wider = 0, taller = 0, quality = "high", ...) {
+save_figure = function(gg, out_path, taller = 0, page_width = 6.5, quality = "high", ...) {
   dpi = if (quality == "high") 300 else if (quality == "medium") 200 else 100
   ggplot2::ggsave(
     filename = out_path,
     plot = gg, 
-    width = 7 + wider, 
+    width = page_width, 
     height = 5 + taller,
     units = "in", 
     dpi = dpi,
