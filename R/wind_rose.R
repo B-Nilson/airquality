71a756c5e656b7f56dd074ad401e738d05459f51
Brayden Nilson <nilson.brayden@gmail.com>
2024-09-28 01:46:47 -0700

    abstract cut_wind_speed and allow for a few wd_nbins options

--- R/wind_rose.R
+++ R/wind_rose.R
@@ -1,37 +1,52 @@
-
-get_cardinal_direction = function(wd){
-  wd_step = 22.5
-  half_step = wd_step / 2
-  dir_bins = c(
-    -half_step,
-    seq(half_step, 360 - half_step, by = wd_step),
-    360 + half_step) 
+get_cardinal_direction = function(wd, wd_step = 22.5){
   dir_labels = c(
     "N", "NNE", "NE", "ENE", 
     "E", "ESE", "SE", "SSE", 
     "S", "SSW", "SW", "WSW", 
     "W", "WNW", "NW", "NNW", "N")
-  cut(wd, breaks = dir_bins, labels = dir_labels, right = FALSE)
+  if(wd_step == 22.5) {
+    set = 1:length(dir_labels)
+  }else if(wd_step == 45) {
+    set = seq(1, length(dir_labels), 2)
+  }else if(wd_step == 90) {
+    set = seq(1, length(dir_labels), 4)
+  }else{
+    stop("Provided wd_step is not supported")
+  }
+
+  half_step = wd_step / 2
+  dir_bins = c(
+    -half_step,
+    seq(half_step, 360 - half_step, by = wd_step),
+    360 + half_step) 
+  cut(wd, breaks = dir_bins, labels = dir_labels[set])
 }
 
 wind_rose = function(
     obs, 
     data_cols = c(ws = "ws_1hr_ms", wd = "wd_1hr_degrees"), 
     groups = NULL, 
+    wd_nbins = c(16, 8, 4)[1],
     ws_min = 0,
     ws_step = 2,
     alpha = 0.7) {
   if(is.null(names(data_cols))) names(data_cols) = c("ws", "wd")
-  # Get wind speed bins
-  max_ws = max(obs[[data_cols[1]]], na.rm = TRUE)
-  speed_bins  = unique(c(seq(ws_min, max_ws, by = ws_step), max_ws))
+  if(is.null(names(groups))) names(groups) = groups
+  wd_step = dplyr::case_when(
+    wd_nbins == 16 ~ 22.5,
+    wd_nbins == 8  ~ 45,
+    wd_nbins == 4  ~ 90)
+  if(is.na(wd_step)){
+    stop("wd_step must be either 16, 8, or 4")
+  }
   # Get counts/percents for each ws bin at eac hcardinal direction
   rose_data = obs |>
     dplyr::rename(dplyr::all_of(data_cols)) |>
     dplyr::filter(ws >= ws_min) |>
     dplyr::mutate(
-      ws_bin = ws |> cut(breaks = speed_bins, include.lowest = TRUE),
-      wd_bin = .data$wd |> get_cardinal_direction()) |>
+      ws_bin = .data$ws |>
+        cut_wind_speed(ws_min = ws_min, ws_step = ws_step),
+      wd_bin = get_cardinal_direction(.data$wd, wd_step = wd_step)) |>
     dplyr::group_by(ws_bin, wd_bin, dplyr::across(dplyr::all_of(groups))) |> 
     dplyr::summarise(n = dplyr::n(), .groups = "drop") |>
     dplyr::group_by(dplyr::across(dplyr::all_of(groups))) |>
@@ -47,7 +62,7 @@ wind_rose = function(
   # Make base plot
   gg = ggplot2::ggplot(rose_data) +
     ggplot2::coord_radial(r.axis.inside = TRUE,
-      start = -convert_units(22.5 / 2, "degrees", "radians")) +
+       start = -convert_units(wd_step / 2, "degrees", "radians")) +
     ggplot2::geom_hline(yintercept = most_frequent$p[1] * 1.05) + 
     ggplot2::scale_fill_viridis_d(direction = -1) +
     ggplot2::guides(fill = ggplot2::guide_legend(reverse = TRUE)) +
@@ -73,4 +88,16 @@ wind_rose = function(
       legend.direction = "horizontal", 
       legend.position = "top") +
     ggplot2::labs(fill = "Wind Speed (m/s)")
+}
+
+cut_wind_speed = function(ws, ws_min = 0, ws_step = 2) {
+  max_ws = max(ws, na.rm = TRUE)
+  speed_bins  = seq(ws_min, max_ws, by = ws_step) |> 
+    c(max_ws) |> unique()
+  n_bins = length(speed_bins)
+  speed_labels = paste(
+    speed_bins, "-", dplyr::lead(speed_bins))[1:(n_bins - 1)]
+
+  cut(ws, include.lowest = TRUE, 
+    breaks = speed_bins, labels = speed_labels)
 }
