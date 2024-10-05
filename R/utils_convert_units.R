a7a2f982ed597df5ea87183c31f7b0fc61f12a47
Brayden Nilson <nilson.brayden@gmail.com>
2024-10-04 22:54:00 -0700

    Move convert_units and helps to own file

---
+++ R/utils_convert_units.R
@@ -0,0 +1,75 @@
+# TODO: document
+convert_units = function(x, in_unit, out_unit, y = NULL){
+  # Handle inputs
+  in_unit = toupper(in_unit)
+  out_unit = toupper(out_unit)
+  if (in_unit == out_unit) 
+    return(x)
+
+  # Determine conversion type
+  all_units = all_conversions |> lapply(\(conversions)
+    stringr::str_split(names(conversions), "_to_") |> unlist())
+  conversion_type = sapply(all_units, \(x) in_unit %in% x)
+  conversion_type = names(conversion_type[conversion_type])[1]
+
+  # Convert into shared base unit first if needed
+  base_unit = all_units[[conversion_type]][1]
+  is_base_unit = in_unit == base_unit | out_unit == base_unit
+  if (!is_base_unit) {
+    x = convert_units(x, in_unit, base_unit)
+    in_unit = base_unit
+  }
+  
+  # Apply conversion to desired unit
+  conversion = paste0(in_unit, "_to_", out_unit)
+  conversion_fun = all_conversions[[conversion_type]][[conversion]]
+  if(is.null(y)) conversion_fun(x) else conversion_fun(x, y)
+}
+
+# TODO: add to this
+all_conversions = list(
+  concentrations = list(
+    PPM_to_PPB = function(PPM) PPM * 1000,
+    PPB_to_PPM = function(PPB) PPB / 1000,
+    PPM_to_UGM3 = function(PPM) PPM,
+    UGM3_to_PPM = function(UGM3) UGM3
+  ),
+  temperature = list(
+    C_to_F = function(C) (C * 9 / 5) + 32,
+    C_to_K = function(C) C + 273.15,
+    F_to_C = function(F) (F - 32) * 5 / 9,
+    K_to_C = function(K) K - 273.15
+  ),
+  humidity = list(
+    RH_to_DEWPOINT = function(RH, T){
+      b = ifelse(T >= 0, 17.368, 17.966) # Over water, or over ice
+      c = ifelse(T >= 0, 238.88, 247.15) # Over water, or over ice
+      return(c * log(RH/100 * saturation_vapour_pressure(T) / 6.1121) /
+              (b - log(RH/100 * saturation_vapour_pressure(T) / 6.1121)))
+    },
+    DEWPOINT_to_RH = function(Td, T)
+      saturation_vapour_pressure(Td) / saturation_vapour_pressure(T) * 100
+  ),
+  trigonometry = list(
+    DEGREES_to_RADIANS = function(degrees) {
+      (degrees * pi / 180) %% (2 * pi)
+    },
+    RADIANS_to_DEGREES = function(radians) {
+      (radians * 180 / pi) %% 360
+    }
+  )
+)
+
+saturation_vapour_pressure = function(temperature_c){
+  if(!dplyr::between(temperature_c, -80, 50))
+    warning("Saturation vapour pressure estimation method only optimized within [-80, 50] celcius")
+  # Using the Arden Buck equation (Buck, 1996)
+  e = ifelse(temperature_c > 0,
+             6.1121 * exp( # over water
+               (18.678 - temperature_c/234.5) *
+               (temperature_c/(257.14 + temperature_c))),
+             6.1115 * exp( # over ice
+               (23.036 - temperature_c/333.7) *
+               (temperature_c/(279.82 + temperature_c))))
+  return(e) # units hPa (millibars)
+}
