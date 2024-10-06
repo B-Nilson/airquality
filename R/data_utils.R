85b07de4068703dc35ec08fd71b953d57513abdb
Brayden Nilson <nilson.brayden@gmail.com>
2024-10-05 17:06:27 -0700

    cleanup get_station_data functions

--- R/data_utils.R
+++ R/data_utils.R
@@ -7,16 +7,20 @@
 #' Gather air quality observations from multiple networks and data sources
 #'
 #' @param locations A character vector with at least one value that indicates a
-#' location on Open Street Map that data is desired for (ie. "Prince George, BC, Canada", or "Canada"),
-#' OR an sf object with polygon(s) indicating area of interest.
+#'   location on Open Street Map that data is desired for (ie. "Prince George, BC, Canada", or "Canada"),
+#'   OR an sf object with polygon(s) indicating area of interest.
 #' @param date_range A datetime vector (or a character vector with UTC dates in "YYYY-MM-DD HH" format) with either 1 or 2 values.
-#' Providing a single value will return data for that hour only,
-#' whereas two values will return data between (and including) those times.
-#' @param buffer_dist (Optional) A single numeric value indicating the distance to buffer the station search location by (typically units of km). Default is 10.
-#' @param networks (Optional) A character vector indicating which monitoring networks to get data for. Default is "all".
-#' @param sources (Optional) A character vector indicating which data sources to get data from. Default is "all".
+#'   Providing a single value will return data for that hour only,
+#'   whereas two values will return data between (and including) those times.
+#' @param buffer_dist (Optional) A single numeric value indicating the distance 
+#'   to buffer the station search location by (typically units of km). 
+#'   Default is 10.
+#' @param networks (Optional) A character vector indicating which monitoring networks to get data for. 
+#'   Default is "all".
+#' @param sources (Optional) A character vector indicating which data sources to get data from. 
+#'   Default is "all".
 #' @param verbose (Optional) A single logical (TRUE or FALSE) value indicating if
-#' non-critical messages/warnings should be printed
+#'   non-critical messages/warnings should be printed
 #'
 #' @description
 #' This is the general use function for gathering air quality observation data in a
@@ -65,114 +69,99 @@ get_station_data = function(locations, date_range, buffer_dist = 10,
   if(any(networks == "all")) networks = c("FEM")#, "LCM")
   if(any(sources == "all")) sources = c("BCgov", "ABgov", "AirNow")#, "PurpleAir")
   date_range = handle_date_range(date_range)
+  # Get polygons of locations to search for stations within
   # TODO: allow for station ids/names
-  # Handle locations input being a vector of names
   if(is.character(locations)){
-    if("North America" %in% locations)
-      locations = c(locations[locations != "North America"],
-        "Canada", "United States", "Mexico")
-    # Get polygons from OSM for desired locations
-    search_area = locations |>
-      lapply_and_bind(get_location_polygons)
+    search_area = lapply_and_bind(locations, get_location_polygons)
     if(is.null(search_area))
       stop(paste0("Unable to find a polygonal boundary for specified location."))
-  # Handle locations input being a sf object
   }else if("sf" %in% class(locations)){
-    if(buffer_dist > 0 & verbose) warning(paste(
-      "Adding a search buffer of", buffer_dist, "km to each location (see arg `buffer_dist`)"))
     search_area = locations
-  }else{
+  }else
     stop("Not sure how to handle provided `locations`")
-  }
-  # Add buffer to search if desired
+  
   sf::st_agr(search_area) = "constant"
-  if(buffer_dist > 0) search_area = sf::st_buffer(search_area, buffer_dist)
+  if (buffer_dist > 0) {
+    if(verbose) 
+      warning(paste("Adding a search buffer of", buffer_dist, 
+        "km to each location (see arg `buffer_dist`)"))
+    search_area = sf::st_buffer(search_area, buffer_dist)
+  }
 
-  # Get station metadata during period
+  # Get station metadata during period in our search area
   data_funs = get_data_collection_funs(networks, sources)
   dates = seq(date_range[1], date_range[2], "30 days")
   stations = lapply_and_bind(names(data_funs), \(net)
     lapply_and_bind(names(data_funs[[net]]), \(src)
       on_error(return = NULL, msg = TRUE,
         data_funs[[net]][[src]]$meta(dates) |>
-        dplyr::mutate(source = src, network = net)))) |>
-    dplyr::filter(!is.na(.data$lat), !is.na(.data$lng)) # TODO: handle in each meta function instead
-
-  # Filter to stations in our search area
-  stations = sf::st_as_sf(stations, coords = c("lng", "lat"), crs = "WGS84")
+          dplyr::mutate(source = src, network = net)))) |>
+    # TODO: handle in each meta function instead
+    dplyr::filter(!is.na(.data$lat), !is.na(.data$lng)) |>
+    sf::st_as_sf(coords = c("lng", "lat"), crs = "WGS84")
   sf::st_agr(stations) = "constant"
   stations = stations |>
     sf::st_intersection(search_area) |>
     dplyr::select('site_id', 'site_name', 'network', 'source', 'geometry')
-
-  if(nrow(stations) == 0){
+  if(nrow(stations) == 0)
     stop("No stations in location(s) and date range for selected networks/sources.")
-  }
-
+  
   # Get data for our stations/date_range
   data = lapply_and_bind(unique(stations$network), \(net)
     lapply_and_bind(names(data_funs[[net]]), \(src){ 
-      site_ids = unique(dplyr::filter(stations, .data$source == src & .data$network == net)$site_id)
-      if(length(site_ids) == 0) return(NULL) # TODO: handle this in each data fun instead of here
-      if(verbose) message(paste(net, "-", src, ":", length(site_ids), 
+      site_ids = unique(stations |>
+        dplyr::filter(.data$source == src & .data$network == net) |>
+        dplyr::pull(.data$site_id))
+      if(length(site_ids) == 0) return(NULL) 
+      
+      if(verbose) message(paste(
+        net, "-", src, ":", length(site_ids), 
         "station(s) to check for data"))
+      data_fun = data_funs[[net]][[src]]$data
       on_error(return = NULL, msg = TRUE,
-        data_funs[[net]][[src]]$data(stations = site_ids, date_range) |>
-          dplyr::mutate(source = src, network = net)) })) 
-  
-  return(list(stations = stations, data = data))
+        data_fun(stations = site_ids, date_range, verbose = verbose) |>
+          dplyr::mutate(source = src, network = net))})) 
+  list(stations = stations, data = data)
+}
+
+# TODO: Document
+get_location_polygons = function(location_name, verbose = TRUE) {
+  on_error(return = NULL, msg = verbose, 
+    osmdata::getbb(location_name, format_out = "sf_polygon"))
 }
 
 get_data_collection_funs = function(networks, sources){
-  data_funs = list(
-    # Federal Equivalent Method monitors
-    FEM = list(
-      # Canada Province of BC
-      BCgov = list(data = get_bcgov_data, meta = get_bcgov_stations),
-      # Canada Province of BC
-      ABgov = list(data = get_abgov_data, meta = get_abgov_stations),
-      # USA (and elsewhere...) AirNow
-      AirNow = list(data = get_airnow_data, meta = get_airnow_stations)
-    )#,
-    # Low-Cost Monitors
-    # LCM = list(
-    #   # PurpleAir PM monitors
-    #   PurpleAir = list(data = get_purpleair_data, meta = get_purpleair_stations)
-    # )
-  )
-  data_funs = data_funs[networks] |>
+  list(
+    FEM = list( # Federal Equivalent Method monitors
+      BCgov  = list(data = get_bcgov_data , meta = get_bcgov_stations),
+      ABgov  = list(data = get_abgov_data , meta = get_abgov_stations),
+      AirNow = list(data = get_airnow_data, meta = get_airnow_stations)),
+    LCM = list( # Low-Cost Monitors
+      PurpleAir = list(data = get_purpleair_data, meta = get_purpleair_stations))
+  )[networks] |>
     lapply(\(srcs) srcs[names(srcs) %in% sources])
-
-  return(data_funs)
 }
 
 data_citation = function(source){
-  data_sources = list(
-    BCgov = "the British Columbia Ministry of Environment and Climate Change Strategy",
-    ABgov = "the Alberta Ministry of Environment and Protected Areas",
-    AirNow = "the US Environmental Protection Agency",
-    PurpleAir = "PurpleAir"
-  )
-  data_urls = list(
-    BCgov = "https://www2.gov.bc.ca/gov/content/environment/air-land-water/air",
-    ABgov = "https://www.alberta.ca/access-air-data",
-    AirNow = "https://www.airnow.gov",
-    PurpleAir = "www.purpleair.com"
-  )
- message(paste0("Data from the '", source,
-        "' repository are collected from ", data_sources[[source]],
-        " and are NOT to be used commercially. ",
-        "Recent observations are not quality assured, ",
-        "and are intended research and/or situational awareness ",
-        "(NOT for regulatory decision making). ",
-        "See `", data_urls[[source]], "` for more information."))
-}
-
-get_location_polygons = function(location_names, verbose = TRUE) {
-  on_error(return = NULL, msg = verbose, {
-    locs = osmdata::getbb(location_names, format_out = "sf_polygon") 
-    locs[!sapply(locs, is.null)] |>
-      dplyr::bind_rows() |>
-      sf::st_cast("POLYGON")
-  })
+  source_meta = list(
+    BCgov = list(
+      name = "the British Columbia Ministry of Environment and Climate Change Strategy",
+      url = "https://www2.gov.bc.ca/gov/content/environment/air-land-water/air"),
+    ABgov = list(
+      name = "the Alberta Ministry of Environment and Protected Areas",
+      url = "https://www.alberta.ca/access-air-data"),
+    AirNow = list(
+      name = "the US Environmental Protection Agency",
+      url = "https://www.airnow.gov"),
+    PurpleAir = list(
+      name = "the PurpleAir API",
+      url = "https://www.purpleair.com"))
+ message(paste0(
+  "Data from the '", source,"'",
+  " repository are collected from ", source_meta[[source]]$name,
+  " and are NOT to be used commercially.",
+  " Recent observations are not quality assured,",
+  " and are intended for research and/or situational awareness",
+  " (**NOT for regulatory decision making**).",
+  " See `", source_meta[[source]]$url, "` for more information."))
 }
