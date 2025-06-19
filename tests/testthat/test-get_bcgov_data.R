36c42ce16606f04917c7712bc6f50fa04b8f7b90
B-Nilson <nilson@unbc.ca>
2025-06-19 15:38:07 -0700

    verbose -> quiet

--- tests/testthat/test-get_bcgov_data.R
+++ tests/testthat/test-get_bcgov_data.R
@@ -1,10 +1,10 @@
-test_that("an error/warning is not thrown in normal usaage", {
+test_that("an error/warning is not thrown in normal usage", {
   date_range <- "2019-02-01 00"
   obs <- expect_no_warning(expect_no_error(
     get_bcgov_data(
       stations = "0450307",
       date_range = date_range,
-      verbose = FALSE
+      quiet = TRUE
     )
   ))
 })
@@ -15,10 +15,10 @@ test_that("returns requested stations only", {
   stations <- c("0450307", "E206898")
   date_range <- "2019-02-01 00"
   # Case: a single station
-  obs <- get_bcgov_data(stations[1], date_range, verbose = FALSE)
+  obs <- get_bcgov_data(stations[1], date_range, quiet = TRUE)
   expect_true(all(unique(obs$site_id) %in% stations[1]))
   # Case: 2+ stations
-  obs <- get_bcgov_data(stations, date_range, verbose = FALSE)
+  obs <- get_bcgov_data(stations, date_range, quiet = TRUE)
   expect_true(all(unique(obs$site_id) %in% stations))
 })
 
@@ -26,9 +26,9 @@ test_that("unknown stations cause warning", {
   stations <- c("bananas", "0450307")
   date_range <- "2019-02-02 00"
   # Case: All stations invalid
-  expect_error(get_bcgov_data(stations[1], date_range, verbose = FALSE))
+  expect_error(get_bcgov_data(stations[1], date_range, quiet = TRUE))
   # Case: Some stations invalid
-  expect_warning(get_bcgov_data(stations, date_range, verbose = FALSE))
+  expect_warning(get_bcgov_data(stations, date_range, quiet = TRUE))
 })
 
 # Inputs: date_range ------------------------------------------------------
@@ -36,12 +36,12 @@ test_that("unknown stations cause warning", {
 test_that("invalid date_range causes error", {
   station <- "0450307"
   # Case: invalid input value
-  expect_error(get_bcgov_data(station, "bananas", verbose = FALSE))
+  expect_error(get_bcgov_data(station, "bananas", quiet = TRUE))
   # Case: too many dates
   expect_error(get_bcgov_data(
     station,
     c("1919-01-01 00", "1919-01-01 01", "1919-01-01 02"),
-    verbose = FALSE
+    quiet = TRUE
   ))
 })
 
@@ -52,11 +52,11 @@ test_that("too early date_range causes warning/error", {
   expect_error(get_bcgov_data(
     station,
     earliest_time - hours(1),
-    verbose = FALSE
+    quiet = TRUE
   ))
   # Case: Partly in the past
   date_range <- c(earliest_time - lubridate::hours(1), earliest_time)
-  expect_warning(get_bcgov_data(station, date_range, verbose = FALSE))
+  expect_warning(get_bcgov_data(station, date_range, quiet = TRUE))
 })
 
 test_that("too late date_range causes warning/error", {
@@ -64,10 +64,10 @@ test_that("too late date_range causes warning/error", {
   current_time <- lubridate::floor_date(Sys.time(), "hours")
   future_time <- current_time + lubridate::hours(24)
   # Case: All in the future
-  expect_error(get_bcgov_data(station, future_time, verbose = FALSE))
+  expect_error(get_bcgov_data(station, future_time, quiet = TRUE))
   # Case: Partly in the future
   date_range <- c(current_time - lubridate::hours(1), future_time)
-  expect_warning(get_bcgov_data(station, date_range, verbose = FALSE))
+  expect_warning(get_bcgov_data(station, date_range, quiet = TRUE))
 })
 
 # Outputs: dates ---------------------------------------------------------
@@ -77,7 +77,7 @@ test_that("all dates non-na and within requested period", {
     c("2019-01-01 00", "2019-01-01 01"),
     tz = "America/Toronto"
   )
-  obs <- get_bcgov_data("0450307", date_range, verbose = FALSE)
+  obs <- get_bcgov_data("0450307", date_range, quiet = TRUE)
   # Case: All date_utc non-NA
   expect_true(all(!is.na(obs$date_utc)))
   # Case: All date_local non-NA
@@ -91,7 +91,7 @@ test_that("date_local converts to date_utc correctly", {
     c("2019-02-01 00", "2019-02-02 00"),
     tz = "America/Vancouver"
   )
-  obs <- get_bcgov_data("0450307", date_range, verbose = FALSE)
+  obs <- get_bcgov_data("0450307", date_range, quiet = TRUE)
   obs <- obs |> convert_date_utc_to_local()
   # Case: date_utc the same as converting date_local to UTC
   expect_equal(obs$date_utc, obs$date_utc_from_local)
@@ -104,10 +104,10 @@ test_that("expected data returned", {
   station <- "0450307"
   date_range <- lubridate::ymd_h(c("2019-02-01 00", "2019-02-02 00"))
 
-  obs <- get_bcgov_data(station, date_range, verbose = FALSE)
+  obs <- get_bcgov_data(station, date_range, quiet = TRUE)
   expect_snapshot(obs)
 
-  obs_raw <- get_bcgov_data(station, date_range, raw = TRUE, verbose = FALSE)
+  obs_raw <- get_bcgov_data(station, date_range, raw = TRUE, quiet = TRUE)
   expect_snapshot(obs_raw)
 })
 
