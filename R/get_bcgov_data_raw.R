a94907315685711636d83ea88e9d5ac96c93bb1b
B-Nilson <nilson.brayden@gmail.com>
2025-08-19 10:44:49 -0700

    cleanup raw data functions

--- R/get_bcgov_data_raw.R
+++ R/get_bcgov_data_raw.R
@@ -12,12 +12,17 @@ bcgov_get_raw_data <- function(stations, variables = "all", quiet = FALSE) {
     STATION_NAME = "character"
   )
 
+  # Standardize common var names
+  variables[variables == "pm2.5"] <- "pm25"
+
+  # Handle "all" in stations
   all_stations <- bcgov_get_raw_stations()
   if (any(stations == "all")) {
     stations <- all_stations
-    collection_mode <- "variables"
+    collection_mode <- "variables" # force variable mode if all stations desired
   }
 
+  # Handle invalid stations
   if(all(!stations %in% all_stations)) {
     stop("All provided stations not available for raw data")
   }
@@ -29,31 +34,32 @@ bcgov_get_raw_data <- function(stations, variables = "all", quiet = FALSE) {
   }
   stations <- stations[stations %in% all_stations]
 
-  variables[variables == "pm2.5"] <- "pm25"
-  
+  # Determine which columns to drop if variables not "all"
+  is_id_col <- names(bcgov_col_names) %in%
+    c('date_utc', 'site_id', 'quality_assured')
   if (any(variables == "all")) {
-    variables_to_drop = character(0)
-    collection_mode <- "stations"
+    cols_to_drop <- character(0)
   } else {
-    variables_to_drop <- bcgov_col_names[
-      !(names(bcgov_col_names) |>
-        stringr::str_starts(variables |> paste0(collapse = "|"))) &
-        !(names(bcgov_col_names) %in%
-          c('date_utc', 'site_id', 'quality_assured'))
+    is_col_in_variables <- names(bcgov_col_names) |>
+      stringr::str_starts(variables |> paste0(collapse = "|"))
+    cols_to_drop <- bcgov_col_names[
+      !is_col_in_variables & !is_id_col
     ] |>
       unname()
   }
 
+  # Force variable mode if only one variable and more than a few stations
   if(length(variables) == 1 & length(stations) > 3) {
     collection_mode <- "variables"
-  }
+  } else if(length(variables) == 2 & length(stations) > 10) {
+    collection_mode <- "variables"
+  } 
 
   # Determine files to download
   if (collection_mode == "variables") {
+    is_instrument_col <- bcgov_col_names |> endsWith("_INSTRUMENT")
     variables <- bcgov_col_names[
-      ! bcgov_col_names %in% variables_to_drop & 
-      ! names(bcgov_col_names) %in% c('date_utc', 'site_id', 'quality_assured') &
-      ! stringr::str_detect(bcgov_col_names, "_INSTRUMENT")
+      !bcgov_col_names %in% cols_to_drop & !is_id_col & !is_instrument_col
     ] |> 
       unname()
     data_paths <- raw_directory_variables |>
@@ -77,7 +83,7 @@ bcgov_get_raw_data <- function(stations, variables = "all", quiet = FALSE) {
             showProgress = !quiet
           ) |>
             bcgov_format_raw_data(mode = collection_mode) |>
-            dplyr::select(-dplyr::any_of(variables_to_drop)) |>
+            dplyr::select(-dplyr::any_of(cols_to_drop)) |>
             handyr::on_error(.return = NULL)
         )
       }
@@ -93,42 +99,40 @@ bcgov_format_raw_data <- function(raw_data, mode = "stations") {
       bcgov_format_qaqc_data(raw_data, use_rounded_value = TRUE)
     )
   }
-  if(mode == "stations") {
-    meta_cols <- c("EMS_ID", "DATE_PST")
-    instrument_cols <- stringr::str_subset(names(raw_data), "_INSTRUMENT$")
-    unit_cols <- stringr::str_subset(names(raw_data), "_UNITS$")
-    value_cols <- unit_cols |>
-      stringr::str_remove("_UNITS$")
-  }else {
-    meta_cols <- c("EMS_ID", "DATE_PST")
+
+  meta_cols <- c("EMS_ID", "DATE_PST")
+  if (mode == "stations") {
+    instrument_cols <- names(raw_data) |> stringr::str_subset("_INSTRUMENT$")
+    unit_cols <- names(raw_data) |> stringr::str_subset("_UNITS$")
+    value_cols <- unit_cols |> stringr::str_remove("_UNITS$")
+  } else {
     instrument_cols <- character(0)
     value_cols <- bcgov_col_names[bcgov_col_names %in% names(raw_data)] |> 
       unname()
     value_cols <- value_cols[!value_cols %in% meta_cols]
-    unit_cols <- paste0(value_cols, "_UNITS")
+    unit_cols <- value_cols |> paste0("_UNITS")
     # Insert default units as unit columns as no units provided
     for(i in 1:length(unit_cols)) { # TODO: confirm units are correct here
+      is_value_col <- bcgov_col_names %in% value_cols[i]
       raw_data[[unit_cols[i]]] <- default_units[
-        names(bcgov_col_names[bcgov_col_names %in% value_cols[i]])
+        names(bcgov_col_names[is_value_col])
       ]
     }
   }
   
   # Assign units to value columns
   for (i in 1:length(unit_cols)) {
-    value_col <- value_cols[i]
-    unit_col <- unit_cols[i]
     default_unit <- default_units[
-      names(bcgov_col_names[bcgov_col_names %in% c(value_col, names(value_col))])
+      names(bcgov_col_names[bcgov_col_names %in% c(value_cols[i], names(value_cols[i]))])
     ]
     raw_data <- raw_data |>
       dplyr::mutate(
         dplyr::across(
-          dplyr::all_of(value_col),
+          dplyr::all_of(value_cols[i]),
           \(x) {
             x |>
               units::set_units(
-                bcgov_fix_units(.data[[unit_col]][1]),
+                bcgov_fix_units(.data[[unit_cols[i]]][1]),
                 mode = "standard"
               ) |> 
               units::set_units(default_unit, mode = "standard")
