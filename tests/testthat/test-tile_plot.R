1ad248a4cb972816635ff9973752d17e0cd110fe
Brayden Nilson <nilson.brayden@gmail.com>
2026-06-19 23:14:57 -0700

    cleanup and enhance tile_plot()

--- tests/testthat/test-tile_plot.R
+++ tests/testthat/test-tile_plot.R
@@ -1,19 +1,13 @@
 test_that("Plot created without error", {
-  obs <- get_bcgov_data(
-    stations = "0450307",
-    date_range = c(Sys.time() - lubridate::days(10), Sys.time()),
-    quiet = TRUE
-  )
-  expect_no_error(
-    obs |>
-      dplyr::mutate(pm25_1hr = as.numeric(pm25_1hr)) |> # drop units
+  expect_no_error(expect_no_warning(
+    example_obs |>
       tile_plot(
-        # TODO: handle z units properly
         x = "hour",
         y = "day",
         z = "pm25_1hr",
         FUN = mean,
-        date_col = "date_utc"
-      )
-  )
+        date_col = "date_local"
+      ) |>
+      print()
+  ))
 })
